9e3049405ec1ca2860d86854abcb173a05db0365
Ryan Gustafson <ryangustafson@Ryans-MacBook-Pro.local>
2022-09-25 21:46:54 -0500

    sudo code

---
+++ script.js
@@ -0,0 +1,12 @@
+// add moment.js
+
+// add time variables
+
+// need to set the time
+
+// need to put the times vars into the local storage 
+
+// have save option to conncet buttons to local storage
+
+// clear options
+
