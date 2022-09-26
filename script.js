bf03bdc83030b7e456e183a58103a8e4984a33a6
Ryan Gustafson <ryangustafson@Ryans-MacBook-Pro.local>
2022-09-25 22:16:24 -0500

    added vars for times in JS

--- script.js
+++ script.js
@@ -1,6 +1,64 @@
 // add moment.js
 
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY")
+var currentHour = moment().format('h:mm:ss a')
+//"a" stands for am
+
 // add time variables
+
+var twelveAm = $("#12am")
+
+var oneAm = $("#01am")
+
+var twoAm = $("#02am")
+
+var threeAm = $("#03am")
+
+var fourAm = $("#04am")
+
+var fiveAm = $("#05am")
+
+var sixAm = $("#06am")
+
+var sevenAm = $("#07am")
+
+var eightAm = $("#08am")
+
+var nineAm = $("#09am")
+
+var tenAm = $("#10am")
+
+var elevenAm = $("#11am")
+
+var twelvePM = $("#12pm")
+
+var onePm = $("#01pm")
+
+var twoPm = $("#02pm")
+
+var threePm = $("#03pm")
+
+var fourPm = $("#04pm")
+
+var fivePm = $("#05pm")
+
+var sixPm = $("#06am")
+
+var sevenPm = $("#07pm")
+
+var eightPm = $("#08pm")
+
+var ninePm = $("#09pm")
+
+var tenPm = $("#10pm")
+
+var elevenPm = $("#11pm")
+
+var userInput
+
+var hourInput
+
+var hour = moment().hours()
 
 // need to set the time
 
@@ -9,4 +67,6 @@
 // have save option to conncet buttons to local storage
 
 // clear options
+
+
 
